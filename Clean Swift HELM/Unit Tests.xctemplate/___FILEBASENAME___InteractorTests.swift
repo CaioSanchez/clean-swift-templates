33551093a7f3322dabde66398425183a4cd9edf7
Caio Sanchez <caiossc@hotmail.com>
2018-07-08 17:09:20 -0300

    Fix unit test templates generation

--- Clean Swift HELM/Unit Tests.xctemplate/___FILEBASENAME___InteractorTests.swift
+++ Clean Swift HELM/Unit Tests.xctemplate/___FILEBASENAME___InteractorTests.swift
@@ -12,42 +12,37 @@
 @testable import ___PROJECTNAME___
 import XCTest
 
-class ___FILEBASENAMEASIDENTIFIER___InteractorTests: XCTestCase
-{
-  // MARK: - Subject under test
-  
-  var sut: ___FILEBASENAMEASIDENTIFIER___Interactor!
-  
-  // MARK: - Test lifecycle
-  
-  override func setUp()
-  {
-    super.setUp()
-    setup___FILEBASENAMEASIDENTIFIER___Interactor()
-  }
-  
-  override func tearDown()
-  {
-    super.tearDown()
-  }
-  
-  // MARK: - Test setup
-  
-  func setup___FILEBASENAMEASIDENTIFIER___Interactor()
-  {
-    sut = ___FILEBASENAMEASIDENTIFIER___Interactor()
-  }
-  
-  // MARK: - Test doubles
-  
-  // MARK: - Tests
-  
-  func testSomething()
-  {
-    // Given
-    
-    // When
-    
-    // Then
-  }
+class ___VARIABLE_sceneName___InteractorTests: XCTestCase {
+    // MARK: - Subject under test
+
+    var sut: ___VARIABLE_sceneName___Interactor!
+
+    // MARK: - Test lifecycle
+
+    override func setUp() {
+        super.setUp()
+        setup___VARIABLE_sceneName___Interactor()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    // MARK: - Test setup
+
+    func setup___VARIABLE_sceneName___Interactor() {
+        sut = ___VARIABLE_sceneName___Interactor()
+    }
+
+    // MARK: - Test doubles
+
+    // MARK: - Tests
+
+    func testSomething() {
+        // Given
+
+        // When
+
+        // Then
+    }
 }
